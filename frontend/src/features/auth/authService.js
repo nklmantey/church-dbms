8a2eac922bca6f7e765c3625001374ef45e75779
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-12-08 09:49:39 +0000

    necessary changes

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,26 +1,26 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = '/api/users/'
+const API_URL = "/users/";
 
 //register user
-const register = async(userData) => {
-    const res = await axios.post(API_URL, userData)
+const register = async (userData) => {
+  const res = await axios.post(API_URL, userData);
 
-    if(res.data) {
-        localStorage.setItem('user', JSON.stringify(res.data))
-    }
+  if (res.data) {
+    localStorage.setItem("user", JSON.stringify(res.data));
+  }
 
-    return res.data
-}
+  return res.data;
+};
 
 //logging out user & destoying token
 const logout = () => {
-    localStorage.removeItem('user')
-}
+  localStorage.removeItem("user");
+};
 
 export const authService = {
-    register,
-    logout
-}
+  register,
+  logout,
+};
 
-export default authService
+export default authService;
